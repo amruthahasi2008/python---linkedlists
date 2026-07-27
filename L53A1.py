894898adf1980d62f9610ad39c951ca50f381bd9
Sri Sai Amrutha Hasini Adunutala <amruthahasini2008@gmail.com>
2026-07-27 11:37:31 +0530

    Add files via upload

---
+++ L53A1.py
@@ -0,0 +1,13 @@
+#Instialising matrices
+x = [[8,2,6],
+     [4,1,7]]
+y = [[3,8,10],
+     [9,15,9]]
+answer = [[0,0,0],
+          [0,0,0]]
+for i in range(len(x)):
+    for j in range(len(x[0])):
+        answer[i][j] = x[i][j]+y[i][j]
+
+for r in answer:
+    print(r)
